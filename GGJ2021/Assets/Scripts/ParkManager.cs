8af044dac068c80178a3d4dfd3569902462c6016
ClearBlueSkies <edmund.lewry@ntlworld.com>
2021-01-31 16:44:48 +0000

    Found me a dog

--- GGJ2021/Assets/Scripts/ParkManager.cs
+++ GGJ2021/Assets/Scripts/ParkManager.cs
@@ -1,15 +1,33 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ParkManager : MonoBehaviour
 {
     public GameObject parkPrefab;
+    public GameObject dogPrefab;
+    public int dogSpawnChance = 10;
 
     public void CreateParkSegment(Vector3 position)
     {
         GameObject park = Instantiate(parkPrefab, position, Quaternion.identity);
         park.transform.localScale = new Vector3(0, 0, 0);
+
+        if(ShouldSpawnDog())
+        {
+            SpawnDog(position);
+        }
+    }
+
+    private bool ShouldSpawnDog()
+    {
+        int roll = Random.Range(0, 100);
+        Debug.Log($"Roll was a {roll}, {((roll < dogSpawnChance) ? "" : "not")} spawning a dog");
+        return (roll < dogSpawnChance);
+    }
+
+    private void SpawnDog(Vector3 position)
+    {
+        Vector3 dogPosition = position + new Vector3(0, 3, 15);
+        GameObject dog = Instantiate(dogPrefab, dogPosition, Quaternion.identity);
+        dog.transform.localScale = new Vector3(0, 0, 0);
     }
 }
